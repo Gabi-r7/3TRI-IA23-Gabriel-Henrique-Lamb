1e12165a3525f9a197fa25010c905be65d24282f
Gabriel Henrique Lamb <120254391+Gabi-r7@users.noreply.github.com>
2023-11-14 19:57:20 +0000

    Refactor CSS and JS code

--- 001 - ATIVIDADE - Menu Hamburger/main.js
+++ 001 - ATIVIDADE - Menu Hamburger/main.js
@@ -1,33 +1,59 @@
-window.addEventListener("scroll", ev => {
-  const scroll = document.documentElement.scrollTop
-  const height = document.documentElement.scrollHeight - document.documentElement.clientHeight
-  const percent = (scroll / height) * 100
-  document.body.style.setProperty("--_percent",  percent+"%")
-  
-})
+const sections = document.querySelectorAll('section');
+const h1eh2 = document.querySelectorAll('h1, h2, .meuBtn');
 
+window.addEventListener("load", function() {
+  h1eh2.forEach(elemento => { elemento.style.background = 'blue'; });
+  sections.forEach(section => { section.style.boxShadow = `0 0 40px blue`; });
+});
+
+window.addEventListener("scroll", ev => {
+  const scroll = document.documentElement.scrollTop;
+  const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  const percent = (scroll / height) * 100;
+  console.log(percent);
+  // Seleciona todas as seções
+  alterarCor(percent);
+
+  // Itera sobre todas as seções e altera a cor do box-shadow
+  sections.forEach(section => alterarCorBoxShadow(section));
+  h1eh2.forEach(h1 => alterarBackground(h1));
+  h1eh2.forEach(h2 => alterarBackground(h2));
+
+});
+
+let cor;
+function alterarCor(percent) {
+
+  if (percent < 25) {
+    cor = 'blue';
+  } else if (percent < 50) {
+    cor = 'purple';
+  } else if (percent < 75) {
+    cor = 'green';
+  } else {
+    cor = 'red';
+  }
+}
+
+function alterarBackground(elemento) {
+  elemento.style.transition = 'background 0.7s ease-in-out';
+  elemento.style.background = cor;
+}
+
+function alterarCorBoxShadow(elemento) {
+  elemento.style.transition = 'box-shadow 0.7s ease-in-out';
+  elemento.style.boxShadow = `0 0 40px ${cor}`;
+}
 
 // ----------------------------------------------------------------------------------
 
-const btHamburger = document.querySelector(".menu-hamburger")
-const mainMenu = document.querySelector("nav.main-menu")
+const btHamburger = document.querySelector(".menu-hamburger");
+const mainMenu = document.querySelector("nav.main-menu");
 
 btHamburger.addEventListener("click", () => {
-  btHamburger.classList.toggle("opened")
-  mainMenu.classList.toggle("opened")
-})
-
-// ----------------------------------------------------------------------------------
-
-const btDialog = document.querySelector(".bt-dialog")
-const btModal = document.querySelector(".bt-modal")
-const dialog = document.querySelector("dialog")
-
-btDialog.addEventListener("click", () => dialog.show())
-btModal.addEventListener("click", () => dialog.showModal())
-
-document.querySelectorAll("dialog button").forEach(
-  bt => bt.addEventListener("click", () => dialog.close()))
+  btHamburger.classList.toggle("opened");
+  mainMenu.classList.toggle("opened");
+});
 
 // ----------------------------------------------------------------------------------
 
